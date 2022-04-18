c58e482d675b4d5997e0120ea74534e7d64ee87c
Muhannad <102443619+Muhannad0101@users.noreply.github.com>
2022-04-18 07:49:16 +0300

    Add files via upload

---
+++ AB_Testing_on_mood/Query_1/Query-1.sql
@@ -0,0 +1,7 @@
+-- Returns first 100 rows from dsv1069.final_assignments
+
+--DIRECTIONS: Modify this query so that it counts 
+--the number of rows in the table dsv1069.users
+SELECT 
+   *
+   FROM dsv1069.final_assignments_qa
